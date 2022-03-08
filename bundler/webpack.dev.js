eca1d6949653d5daa79e5ea173b038e54291e6c2
Huy <huyph14494@gmail.com>
2022-03-09 00:05:06 +0700

    update

--- bundler/webpack.dev.js
+++ bundler/webpack.dev.js
@@ -14,13 +14,13 @@ module.exports = merge(
         mode: 'development',
         devServer:
         {
-            host: '0.0.0.0',
+            host: '127.0.0.1', // 0.0.0.0'
             port: portFinderSync.getPort(8080),
             contentBase: './dist',
             watchContentBase: true,
             open: true,
             https: false,
-            useLocalIp: true,
+            useLocalIp: false, // true
             disableHostCheck: true,
             overlay: true,
             noInfo: true,
@@ -28,11 +28,11 @@ module.exports = merge(
             {
                 const port = server.options.port
                 const https = server.options.https ? 's' : ''
-                const localIp = ip.v4.sync()
-                const domain1 = `http${https}://${localIp}:${port}`
+                // const localIp = ip.v4.sync()
+                // const domain1 = `http${https}://${localIp}:${port}`
+
                 const domain2 = `http${https}://localhost:${port}`
-                
-                console.log(`Project running at:\n  - ${infoColor(domain1)}\n  - ${infoColor(domain2)}`)
+                console.log(`Project running at:\n  - ${infoColor(domain2)}`)
             }
         }
     }
